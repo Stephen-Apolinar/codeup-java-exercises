788aec49e6d33240966ce0957f560cd0bab86615
Stephen Apolinar <stephenapolinar@Stephens-MacBook-Pro.local>
2018-08-06 16:56:24 -0500

    exceptions

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -3,9 +3,8 @@ package util;
 public class InputTest {
     public static void main(String[] args) {
         Input input = new Input();
-
-        input.getString(input.getString());
-        input.getString("helllo");
+        System.out.println("pick an integer");
+        input.getDouble();
         System.out.println(input);
     }
 }
